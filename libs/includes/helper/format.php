980cf3131a82fb88e4dda4a0b5a1573bf583cc4a
chris <code@cre8.info>
2019-04-23 16:58:06 +0200

    better character replacement

--- libs/includes/helper/format.php
+++ libs/includes/helper/format.php
@@ -7,6 +7,10 @@ use ATFApp\Core AS Core;
 class Format {
 	
 	private static $stringNone = '--';
+    // only number, characters and dash (-)
+    private static $filenameAllowedCharacters = '/[^\w\d\-]/';
+    // only number, characters, dash (-) and dot (.)
+    private static $filenameAllowedCharactersDots = '/[^\w\d\-\.]/';
 	
 	public function __construct() { }
 	
@@ -78,19 +82,10 @@ class Format {
 	 * @return string
 	 */
 	public static function cleanupText($text, $removeSpaces=true) {
-		$chars2replace = [
-		    'Š'=>'S', 'š'=>'s', 'Ð'=>'Dj','Ž'=>'Z', 'ž'=>'z', 'À'=>'A', 'Á'=>'A', 'Â'=>'A', 'Ã'=>'A', 'Ä'=>'A', 
-		    'Å'=>'A', 'Æ'=>'A', 'Ç'=>'C', 'È'=>'E', 'É'=>'E', 'Ê'=>'E', 'Ë'=>'E', 'Ì'=>'I', 'Í'=>'I', 'Î'=>'I', 
-		    'Ï'=>'I', 'Ñ'=>'N', 'Ò'=>'O', 'Ó'=>'O', 'Ô'=>'O', 'Õ'=>'O', 'Ö'=>'O', 'Ø'=>'O', 'Ù'=>'U', 'Ú'=>'U', 
-		    'Û'=>'U', 'Ü'=>'U', 'Ý'=>'Y', 'Þ'=>'B', 'ß'=>'ss','à'=>'a', 'á'=>'a', 'â'=>'a', 'ã'=>'a', 'ä'=>'a', 
-		    'å'=>'a', 'æ'=>'a', 'ç'=>'c', 'è'=>'e', 'é'=>'e', 'ê'=>'e', 'ë'=>'e', 'ì'=>'i', 'í'=>'i', 'î'=>'i', 
-		    'ï'=>'i', 'ð'=>'o', 'ñ'=>'n', 'ò'=>'o', 'ó'=>'o', 'ô'=>'o', 'õ'=>'o', 'ö'=>'o', 'ø'=>'o', 'ù'=>'u', 
-		    'ú'=>'u', 'û'=>'u', 'ü'=>'u', 'ý'=>'y', 'ý'=>'y', 'þ'=>'b', 'ÿ'=>'y', 'ƒ'=>'f'
-		];
-		
-		$text = strtr($text, $chars2replace);
+		$text = self::replaceSpecialChars($text);
+
 		$text = str_replace('&', '+', $text);  // replace '&'
-	    $text = trim(preg_replace('/[^\w\d_ -]/si', '', $text)); //remove all illegal chars
+	    $text = trim(preg_replace('/[^\w\d\_ \-\+]/si', '', $text)); //remove all illegal chars
 	    
 	    if ($removeSpaces) {
 	    	$text = str_replace(' -', '-', $text);
@@ -144,4 +139,84 @@ class Format {
 		}
 		return $value;
 	}
+
+
+	    /**
+     * remove non whitelist characters
+     * 
+     * @param string $filename
+     * @param boolean $dotsAllowed allow dots (for filename with extensions)
+     * @param string $replaceWith character to replace with
+     * @return string
+     */
+    public static function cleanupFilename($filename, $dotsAllowed=false, $replaceWith='') {
+        $filename = self::replaceSpecialChars($filename);
+        
+        if ($dotsAllowed) {
+            return preg_replace(self::$filenameAllowedCharactersDots , $replaceWith, $filename);
+        } else {
+            return preg_replace(self::$filenameAllowedCharacters , $replaceWith, $filename);
+        }
+    }
+
+
+    /**
+     * replace things like accents, umlauts and other special chars
+	 * 
+	 * @param string $str
+	 * @return string
+     */
+    public static function replaceSpecialChars($str) {
+      $a = [
+          'À', 'Á', 'Â', 'Ã', 'Ä', 'Å', 'Æ', 'Ç', 'È', 'É', 
+          'Ê', 'Ë', 'Ì', 'Í', 'Î', 'Ï', 'Ð', 'Ñ', 'Ò', 'Ó', 
+          'Ô', 'Õ', 'Ö', 'Ø', 'Ù', 'Ú', 'Û', 'Ü', 'Ý', 'ß', 
+          'à', 'á', 'â', 'ã', 'ä', 'å', 'æ', 'ç', 'è', 'é', 
+          'ê', 'ë', 'ì', 'í', 'î', 'ï', 'ñ', 'ò', 'ó', 'ô', 
+          'õ', 'ö', 'ø', 'ù', 'ú', 'û', 'ü', 'ý', 'ÿ', 'Ā', 
+          'ā', 'Ă', 'ă', 'Ą', 'ą', 'Ć', 'ć', 'Ĉ', 'ĉ', 'Ċ', 
+          'ċ', 'Č', 'č', 'Ď', 'ď', 'Đ', 'đ', 'Ē', 'ē', 'Ĕ', 
+          'ĕ', 'Ė', 'ė', 'Ę', 'ę', 'Ě', 'ě', 'Ĝ', 'ĝ', 'Ğ', 
+          'ğ', 'Ġ', 'ġ', 'Ģ', 'ģ', 'Ĥ', 'ĥ', 'Ħ', 'ħ', 'Ĩ', 
+          'ĩ', 'Ī', 'ī', 'Ĭ', 'ĭ', 'Į', 'į', 'İ', 'ı', 'Ĳ', 
+          'ĳ', 'Ĵ', 'ĵ', 'Ķ', 'ķ', 'Ĺ', 'ĺ', 'Ļ', 'ļ', 'Ľ', 
+          'ľ', 'Ŀ', 'ŀ', 'Ł', 'ł', 'Ń', 'ń', 'Ņ', 'ņ', 'Ň', 
+          'ň', 'ŉ', 'Ō', 'ō', 'Ŏ', 'ŏ', 'Ő', 'ő', 'Œ', 'œ', 
+          'Ŕ', 'ŕ', 'Ŗ', 'ŗ', 'Ř', 'ř', 'Ś', 'ś', 'Ŝ', 'ŝ', 
+          'Ş', 'ş', 'Š', 'š', 'Ţ', 'ţ', 'Ť', 'ť', 'Ŧ', 'ŧ', 
+          'Ũ', 'ũ', 'Ū', 'ū', 'Ŭ', 'ŭ', 'Ů', 'ů', 'Ű', 'ű', 
+          'Ų', 'ų', 'Ŵ', 'ŵ', 'Ŷ', 'ŷ', 'Ÿ', 'Ź', 'ź', 'Ż', 
+          'ż', 'Ž', 'ž', 'ſ', 'ƒ', 'Ơ', 'ơ', 'Ư', 'ư', 'Ǎ', 
+          'ǎ', 'Ǐ', 'ǐ', 'Ǒ', 'ǒ', 'Ǔ', 'ǔ', 'Ǖ', 'ǖ', 'Ǘ', 
+          'ǘ', 'Ǚ', 'ǚ', 'Ǜ', 'ǜ', 'Ǻ', 'ǻ', 'Ǽ', 'ǽ', 'Ǿ', 
+          'ǿ'];
+
+      $b = [
+          'A', 'A', 'A', 'A', 'A', 'A', 'AE', 'C', 'E', 'E', 
+          'E', 'E', 'I', 'I', 'I', 'I', 'D', 'N', 'O', 'O', 
+          'O', 'O', 'O', 'O', 'U', 'U', 'U', 'U', 'Y', 's', 
+          'a', 'a', 'a', 'a', 'a', 'a', 'ae', 'c', 'e', 'e', 
+          'e', 'e', 'i', 'i', 'i', 'i', 'n', 'o', 'o', 'o', 
+          'o', 'o', 'o', 'u', 'u', 'u', 'u', 'y', 'y', 'A', 
+          'a', 'A', 'a', 'A', 'a', 'C', 'c', 'C', 'c', 'C', 
+          'c', 'C', 'c', 'D', 'd', 'D', 'd', 'E', 'e', 'E', 
+          'e', 'E', 'e', 'E', 'e', 'E', 'e', 'G', 'g', 'G', 
+          'g', 'G', 'g', 'G', 'g', 'H', 'h', 'H', 'h', 'I', 
+          'i', 'I', 'i', 'I', 'i', 'I', 'i', 'I', 'i', 'IJ', 
+          'ij', 'J', 'j', 'K', 'k', 'L', 'l', 'L', 'l', 'L', 
+          'l', 'L', 'l', 'l', 'l', 'N', 'n', 'N', 'n', 'N', 
+          'n', 'n', 'O', 'o', 'O', 'o', 'O', 'o', 'OE', 'oe', 
+          'R', 'r', 'R', 'r', 'R', 'r', 'S', 's', 'S', 's', 
+          'S', 's', 'S', 's', 'T', 't', 'T', 't', 'T', 't', 
+          'U', 'u', 'U', 'u', 'U', 'u', 'U', 'u', 'U', 'u', 
+          'U', 'u', 'W', 'w', 'Y', 'y', 'Y', 'Z', 'z', 'Z', 
+          'z', 'Z', 'z', 's', 'f', 'O', 'o', 'U', 'u', 'A', 
+          'a', 'I', 'i', 'O', 'o', 'U', 'u', 'U', 'u', 'U', 
+          'u', 'U', 'u', 'U', 'u', 'A', 'a', 'AE', 'ae', 'O', 
+          'o'];
+      
+      
+      return str_replace($a, $b, $str);
+    }
+
 }
